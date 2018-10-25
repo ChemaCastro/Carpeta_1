05b4e3f6195100fd70dfb4455278df0869a66b41
Chema Castro <chema_cs@yahoo.es>
2018-10-25 12:43:15 +0200

    Fin seccion 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  // Definimos esta propiedad pero no está inicializada, ojo al JavaScript
+  producto: ProductoDescripcion;
+  idProducto: string;
+
+  constructor( private route: ActivatedRoute,
+               public productoService: ProductosService) { }
 
   ngOnInit() {
+
+    this.route.params
+    .subscribe( parametros => {
+
+      console.log(parametros['id']);
+
+      this.productoService.getProducto(parametros['id'])
+            .subscribe( (producto: ProductoDescripcion) => {
+              this.producto = producto;
+              this.idProducto = parametros['id'];
+              // console.log(producto);
+            });
+    });
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { resolve } from 'url';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,76 @@ export class ProductosService {
 
   cargando = true;
   producto: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
     this.cargarProductos();
    }
 
   private cargarProductos() {
-    this.http.get('https://mi-proyecto-en-angular.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[]) => {
-      this.producto = resp;
-      console.log(resp);
-      // SÓLO para probar que funciona el "loading" ponemos un Timeout...
-      // setTimeout(() => {
-      this.cargando = false;
-    // }, 2000);
+
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise( (resolve, reject) => {
+
+      this.http.get('https://mi-proyecto-en-angular.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+        this.producto = resp;
+        console.log(resp);
+        // SÓLO para probar que funciona el "loading" ponemos un Timeout...
+        // setTimeout(() => {
+        this.cargando = false;
+        // }, 2000);
+        resolve();
+      });
+
     });
+  }
+
+  public getProducto( id: string ) {
+
+    return this.http.get(`https://mi-proyecto-en-angular.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  public buscarProducto( termino: string) {
+
+    if ( this.producto.length === 0 ) {
+      // No hay productos cargados por lo que los cargamos
+      this.cargarProductos().then( () => {
+      // Ejecutamos después de tener los productos (.then())
+      // Aplicamos el filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      // Aplicamos el filtro
+      this.filtrarProductos ( termino );
+    }
+
+    this.productosFiltrado = this.productosFiltrado.filter( producto => {
+       return true;
+    });
+
+    // console.log( this.productosFiltrado );
+
+  }
+
+  private filtrarProductos ( termino: string) {
+
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.producto.forEach( prod => {
+
+      const categoriaLow = prod.categoria.toLocaleLowerCase();
+      const tituloLow = prod.titulo.toLocaleLowerCase();
+
+      if ( categoriaLow.indexOf( termino) >= 0 ||
+           tituloLow.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push(prod);
+      }
+
+    });
+
   }
 }
